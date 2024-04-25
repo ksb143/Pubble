0d4da35afadcd7cef5f503cbd474b66a5318ef91
gysim <qysim@naver.com>
2024-04-25 21:22:28 +0900

    feat: 기초 파일 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Routes, Route, useNavigate} from 'react-router-dom';
 import Test from '@/pages/test'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() { 
@@ -15,7 +14,7 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
