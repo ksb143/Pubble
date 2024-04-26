2bff90f3da78079828bcb793e081be86fe1fda5f
김효주 <hyojukim.dev@gmail.com>
2024-04-26 13:18:28 +0900

    Hotfix: Spring 내부 포트 9091로 변경 #1 - Jenkins 내부 기본 포트와 Spring 기본 내부 포트가 8080 으로 동일 - 서버에 올렸을 때 충돌이 발생해서 - Jenkins 내부 포트 9090 으로 변경했지만 - 기본 포트 설정을 다 변경할 수가 없어서 - 8080:tcp가 EC2 에서 자동 실행됨 - Spring 포트를 9091 로 바꿔서 해결 시도 - Docker image 로 만들기 위해서 Dockerfile 작성완료

--- backend/src/main/java/com/ssafy/d109/pubble/PubbleApplication.java
+++ backend/src/main/java/com/ssafy/d109/pubble/PubbleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PubbleApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello Spring");
 		SpringApplication.run(PubbleApplication.class, args);
 	}
 
