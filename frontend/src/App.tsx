792f869ea14e5a7a43f44d0b0ca6befa0c9c3a74
qysim <qysim@naver.com>
2024-04-29 17:32:54 +0900

    Feat: svg 파일 컴포넌트화 설정
    - layout 생성
    - navbar 생성
    - svg 컴포넌트화 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import User from '@/routers/User';
+import Layout from '@/components/layouts/Layout';
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* 상단바 없는 페이지 */}
         <Route path='/user/*' element={<User />} />
+        {/* 상단바 있는 페이지 */}
+        <Route element={<Layout />}>
+          <Route path='/' />
+        </Route>
       </Routes>
     </Router>
   );
---
+++ frontend/src/components/layouts/Navbar.tsx
@@ -0,0 +1,13 @@
+import logo from '@/assets/images/logo_long.png';
+import Envelope from '@/components/icons/envelope.svg?react';
+
+const Navbar = () => {
+  return(
+    <div className="w-full h-16 flex items-center border border-red-600">
+      <img className='h-12 mx-16 border border-blue-600' src={logo} alt="logo" />
+      <Envelope className='w-8 h-8'/>
+    </div>
+  )
+}
+
+export default Navbar
