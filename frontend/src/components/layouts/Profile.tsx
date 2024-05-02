21d91d2cf15e9e73d854a031c68c63e520e51de8
qysim <qysim@naver.com>
2024-05-02 14:01:24 +0900

    Feat: 상단바 UI 구현
    - Badge.tsx 생성
    - Profile.tsx 생성

---
+++ frontend/src/components/layouts/Profile.tsx
@@ -0,0 +1,11 @@
+const Profile = () => {
+  return (
+    <>
+      <div className='flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-purple-300'>
+        <div className='font-normal text-white'>심기획</div>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
