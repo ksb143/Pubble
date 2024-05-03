78a1efb1882deb00958722ff3d4fd99606bee93a
qysim <qysim@naver.com>
2024-05-03 17:54:25 +0900

    Feat: 알림 컴포넌트 구현 중

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Profile from '@/components/layouts/Profile';
 import Notification from '@/components/navbar/Notification';
 import logo from '@/assets/images/logo_long.png';
@@ -6,6 +7,7 @@ import Envelope from '@/assets/icons/envelope.svg?react';
 import Bell from '@/assets/icons/bell.svg?react';
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const [showNotification, setShowNotification] = useState(false);
 
   const toggleNotification = () => {
@@ -14,11 +16,20 @@ const Navbar = () => {
 
   return (
     <>
-      {showNotification && (
-        <Notification closeNotification={() => setShowNotification(false)} />
-      )}
-      <div className='fixed left-0 right-0 top-0 flex h-16 items-center justify-between bg-white px-16 shadow'>
-        <img className='h-12 cursor-pointer' src={logo} alt='logo' />
+      <Notification
+        show={showNotification}
+        closeNotification={() => setShowNotification(false)}
+      />
+
+      <div className='fixed left-0 right-0 top-0 z-30 flex h-16 items-center justify-between bg-white px-16 shadow'>
+        <img
+          className='h-12 cursor-pointer'
+          src={logo}
+          alt='logo'
+          onClick={() => {
+            navigate('/main');
+          }}
+        />
 
         <div className='flex items-center'>
           <div className='mr-8 flex h-11 w-11 cursor-pointer items-center justify-center rounded hover:bg-gray-900/10'>
@@ -26,9 +37,11 @@ const Navbar = () => {
           </div>
 
           <div
-            className='mr-10 flex h-11 w-11 cursor-pointer items-center justify-center rounded hover:bg-gray-900/10'
+            className={`mr-10 flex h-11 w-11 cursor-pointer items-center justify-center rounded hover:bg-gray-900/10 ${showNotification ? ' bg-gray-900/10' : ''}`}
             onClick={toggleNotification}>
-            <Bell className='h-8 w-8 stroke-gray-900 stroke-2' />
+            <Bell
+              className={`h-8 w-8 fill-gray-900 stroke-gray-900 ${showNotification ? 'stroke-[4]' : 'stroke-2'}`}
+            />
           </div>
 
           <Profile />
--- frontend/src/components/navbar/Notification.tsx
+++ frontend/src/components/navbar/Notification.tsx
@@ -1,24 +1,57 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import Xmark from '@/assets/icons/x-mark.svg?react';
 import Bell from '@/assets/icons/bell.svg?react';
+import Profile from '@/components/layouts/Profile';
 
 interface NotificationProps {
-  closeNotification: () => void; // closeNotification 함수 타입 선언
+  show: boolean;
+  closeNotification: () => void;
 }
 
-const Notification: React.FC<NotificationProps> = ({ closeNotification }) => {
+const dialogStyle = css`
+  height: calc(100vh - 64px);
+`;
+
+const ListItem = ({ text }) => (
+  <li className='flex items-center border-b px-2 py-6 last-of-type:border-none'>
+    <Profile />
+    <div className='px-2'>{text}</div>
+  </li>
+);
+
+const items = Array.from({ length: 10 }, (_, index) => `Test${index + 1}`);
+
+const Notification: React.FC<NotificationProps> = ({
+  show,
+  closeNotification,
+}) => {
   return (
-    <div className='fixed right-0 top-16 z-10 h-full w-1/3 border-2 border-red-600 bg-white p-2'>
-      <div className='flex justify-between'>
-        <div className='flex items-center'>
-          <Bell className='h-8 w-8 stroke-gray-900 stroke-2' />
-          <div className='text-2xl font-normal'>알림</div>
+    <>
+      <div
+        className={`fixed inset-0 transition duration-500 ${show ? 'z-10 bg-gray-900/30' : '-z-10 bg-transparent'}`}
+        onClick={closeNotification}></div>
+
+      <div
+        className={`fixed bottom-0 right-0 top-16 z-20 w-[30%] overflow-y-auto rounded-l bg-white p-4 shadow-lg transition duration-1000 ${show ? 'translate-x-0' : 'translate-x-full'}`}
+        css={dialogStyle}>
+        <div className='mb-4 flex items-center justify-between'>
+          <div className='flex items-center'>
+            <Bell className='mr-2 h-6 w-6 fill-gray-900 stroke-gray-900 stroke-2' />
+            <div className='text-2xl font-normal'>알림</div>
+          </div>
+          <Xmark
+            className='h-8 w-8 stroke-gray-900 stroke-1'
+            onClick={closeNotification}
+          />
         </div>
-        <Xmark
-          className='h-8 w-8 stroke-gray-900'
-          onClick={closeNotification}
-        />
+        <ul className='flex flex-col p-2'>
+          {items.map((item) => (
+            <ListItem key={item} text={item} />
+          ))}
+        </ul>
       </div>
-    </div>
+    </>
   );
 };
 
