47fb8fc131ed2f3bf8b2da641e7d1774fc8993d8
장준영 <wnsdud2889@naver.com>
2024-05-05 20:00:58 +0900

    Feat: context-path: /api & detatch other /api & access log

--- backend/src/main/java/com/ssafy/d109/pubble/controller/RequirementCreateController.java
+++ backend/src/main/java/com/ssafy/d109/pubble/controller/RequirementCreateController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping(path = "/api/requirement")
+@RequestMapping(path = "/requirement")
 @RequiredArgsConstructor
 @CrossOrigin("*")
 public class RequirementCreateController {
