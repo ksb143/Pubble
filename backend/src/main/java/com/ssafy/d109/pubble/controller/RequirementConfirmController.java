6a859b60f93a192bedb7382eac971f1f37b23f30
장준영 <wnsdud2889@naver.com>
2024-05-05 19:24:45 +0900

    Fix: delete /api after merge

--- backend/src/main/java/com/ssafy/d109/pubble/controller/RequirementConfirmController.java
+++ backend/src/main/java/com/ssafy/d109/pubble/controller/RequirementConfirmController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 
 @RestController
-@RequestMapping("/api/requirements")
+@RequestMapping("/requirements")
 @Log4j2
 public class RequirementConfirmController {
 
