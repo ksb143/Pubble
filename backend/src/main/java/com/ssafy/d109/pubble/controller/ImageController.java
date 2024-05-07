20a138870ba393c822576f7cfa9b7c757a2a2223
문수민 <sumny1010@gmail.com>
2024-05-07 10:42:47 +0900

    Feat: 이미지 업로드 - 응답 추가

--- backend/src/main/java/com/ssafy/d109/pubble/controller/ImageController.java
+++ backend/src/main/java/com/ssafy/d109/pubble/controller/ImageController.java
@@ -1,7 +1,9 @@
 package com.ssafy.d109.pubble.controller;
 
 import com.ssafy.d109.pubble.dto.requestDto.ImageUploadRequestDto;
+import com.ssafy.d109.pubble.dto.responseDto.ImageUploadResponseDto;
 import com.ssafy.d109.pubble.service.ImageService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,7 +12,7 @@ import org.springframework.web.multipart.MultipartRequest;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api")
+//@RequestMapping("/api")
 public class ImageController {
 
     private final ImageService imageService;
@@ -23,8 +25,18 @@ public class ImageController {
     public ResponseEntity<?> uploadImage(@ModelAttribute ImageUploadRequestDto dto, @RequestParam("upload") MultipartFile upload) throws IOException {
 
         Integer requestId = dto.getRequirementId();
-        imageService.uploadImage(upload, dto.getRequirementId());
 
-        return null;
+        ImageUploadResponseDto responseDto = new ImageUploadResponseDto();
+
+        String s3Url = imageService.uploadImage(upload, dto.getRequirementId());
+        if (s3Url == null) {
+            responseDto.setMessage("Image Upload Failed");
+            responseDto.setData(false);
+            return new ResponseEntity<>(responseDto, HttpStatus.BAD_REQUEST);
+        }
+
+        responseDto.setMessage("Image Upload Success");
+        responseDto.setData(true);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 }
