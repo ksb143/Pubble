e8d04cc1fcb24f80a0ed2f4e99ae678f5e574497
qysim <qysim@naver.com>
2024-05-14 14:09:31 +0900

    Feat: 페이지정보 store 생성

--- frontend/src/stores/pageInfoStore.ts
+++ frontend/src/stores/pageInfoStore.ts
@@ -4,13 +4,17 @@ import { persist, createJSONStorage } from 'zustand/middleware';
 // 페이지 정보를 저장하기 위한 인터페이스 정의
 interface pageInfoStore {
   projectId: number;
+  projectCode: string;
   projectName: string;
   requirementId: number;
+  requirementCode: string;
   requirementName: string;
   isRichPage: boolean;
   setProjectId: (projectId: number) => void;
+  setProjectCode: (projectCode: string) => void;
   setProjectName: (projectName: string) => void;
   setRequirementId: (requirementId: number) => void;
+  setRequirementCode: (requirementCode: string) => void;
   setRequirementName: (requirementName: string) => void;
   setIsRichPage: (isRichPage: boolean) => void;
 }
@@ -19,13 +23,17 @@ const usePageInfoStore = create(
   persist<pageInfoStore>(
     (set) => ({
       projectId: 0,
+      projectCode: '',
       projectName: '',
       requirementId: 0,
+      requirementCode: '',
       requirementName: '',
       isRichPage: false,
       setProjectId: (projectId) => set({ projectId }),
+      setProjectCode: (projectCode) => set({ projectCode }),
       setProjectName: (projectName) => set({ projectName }),
       setRequirementId: (requirementId) => set({ requirementId }),
+      setRequirementCode: (requirementCode) => set({ requirementCode }),
       setRequirementName: (requirementName) => set({ requirementName }),
       setIsRichPage: (isRichPage) => set({ isRichPage }),
     }),
