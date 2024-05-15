520a5fb958792a73cb0b3d0f09ddd60ca8da26c5
qysim <qysim@naver.com>
2024-05-15 13:03:12 +0900

    Feat: pageInfoStore 수정 및 전체 router에 적용

--- frontend/src/components/requirement/RequirementList.tsx
+++ frontend/src/components/requirement/RequirementList.tsx
@@ -2,15 +2,40 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 // 2. library 관련
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable, getPaginationRowModel, SortingState, getSortedRowModel, ColumnFiltersState, getFilteredRowModel, VisibilityState } from '@tanstack/react-table';
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  getPaginationRowModel,
+  SortingState,
+  getSortedRowModel,
+  ColumnFiltersState,
+  getFilteredRowModel,
+  VisibilityState,
+} from '@tanstack/react-table';
 import { MoreHorizontal } from 'lucide-react';
 // 3. api 관련
 import { getRequirement } from '@/apis/project.ts';
 // 4. store 관련
-import usePageInfoStore from "@/stores/pageInfoStore.ts";
+// import usePageInfoStore from '@/stores/pageInfoStore.ts';
 // 5. components 관련
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
-import { DropdownMenuLabel, DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem, DropdownMenuCheckboxItem } from '@/components/ui/dropdown-menu';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import {
+  DropdownMenuLabel,
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+} from '@/components/ui/dropdown-menu';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Checkbox } from '@/components/ui/checkbox';
@@ -20,133 +45,139 @@ export type Requirement = {
   requirementId: number; // 개별 요구사항 pk
   orderIndex: number; // 개별 요구사항 인덱스 번호
   version: string; // 개별 요구사항의 버전
-  isLock: "u" | "l"; // 개별 요구사항 잠금 여부
-  approval: "u" | "h" | "a"; // 개별 요구사항 승인 여부
+  isLock: 'u' | 'l'; // 개별 요구사항 잠금 여부
+  approval: 'u' | 'h' | 'a'; // 개별 요구사항 승인 여부
   approvalComment: string; // 개별 요구사항 승인 코멘트
   code: string; // 개별 요구사항 코드
   requirementName: string; // 개별 요구사항 이름
   detail: string; // 개별 요구사항 상세설명
   manager: {
-      userId: number; // 개별 요구사항 담당자의 사용자 아이디
-      name: string; // 개별 요구사항 담당자의 이름
-      employeeId: string; // 개별 요구사항 담당자의 사번
-      department: string; // 개별 요구사항 담당자의 부서
-      position: string; // 개별 요구사항 담당자의 직위
-      role: string; // 개별 요구사항 담당자의 역할
-      isApprovable: "y" | "n"; // 개별 요구사항 담당자의 승인 가능 여부
-      profileColor: string; // 개별 요구사항 담당자의 프로필 색상
+    userId: number; // 개별 요구사항 담당자의 사용자 아이디
+    name: string; // 개별 요구사항 담당자의 이름
+    employeeId: string; // 개별 요구사항 담당자의 사번
+    department: string; // 개별 요구사항 담당자의 부서
+    position: string; // 개별 요구사항 담당자의 직위
+    role: string; // 개별 요구사항 담당자의 역할
+    isApprovable: 'y' | 'n'; // 개별 요구사항 담당자의 승인 가능 여부
+    profileColor: string; // 개별 요구사항 담당자의 프로필 색상
   };
-  }
+};
 
 interface RequirementListProps {
-    projectId: string // 프로젝트 pk
-    projectCode: string // 프로젝트 코드
-    projectName: string // 프로젝트 이름
-
+  projectId: string; // 프로젝트 pk
+  projectCode: string; // 프로젝트 코드
+  projectName: string; // 프로젝트 이름
 }
 // cloumn 정의
 export const columns: ColumnDef<Requirement>[] = [
   // useMemo는 계산 결과를 메모리에 저장(캐싱)하여, 컬럼 정의를 한번만 하게하고, 불필요한 리렌더링을 방지합니다.
-  
-    {
-      id: "select",
+
+  {
+    id: 'select',
     header: ({ table }) => (
       <Checkbox
         checked={
           table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
+          (table.getIsSomePageRowsSelected() && 'indeterminate')
         }
-        onCheckedChange={(value: boolean) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
+        onCheckedChange={(value: boolean) =>
+          table.toggleAllPageRowsSelected(!!value)
+        }
+        aria-label='Select all'
       />
     ),
     cell: ({ row }) => (
       <Checkbox
         checked={row.getIsSelected()}
         onCheckedChange={(value: boolean) => row.toggleSelected(!!value)}
-        aria-label="Select row"
+        aria-label='Select row'
       />
     ),
     enableSorting: false,
     enableHiding: false,
+  },
+  {
+    accessorKey: 'approvalStatus',
+    header: '승인여부',
+    cell: (info) => info.getValue(),
+  },
+  {
+    accessorKey: 'lockStatus',
+    header: '잠금여부',
+    cell: (info) => info.getValue(),
+  },
+  {
+    accessorKey: 'requirementId',
+    header: '항목ID',
+    cell: (info) => info.getValue(),
+  },
+  {
+    accessorKey: 'requirementName',
+    header: '요구사항이름',
+    cell: (info) => info.getValue(),
+  },
+  {
+    accessorKey: 'description',
+    header: '상세설명',
+    cell: (info) => info.getValue(),
+  },
+  {
+    accessorKey: 'assignee',
+    header: '담당자',
+    cell: (info) => info.getValue(),
+  },
+  {
+    accessorKey: 'author',
+    header: '작성자',
+    cell: (info) => info.getValue(),
+  },
+  {
+    accessorKey: 'currentVersion',
+    header: '현재버전',
+    cell: (info) => info.getValue(),
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const requirement = row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='ghost' className='h-8 w-8 p-0'>
+              <span className='sr-only'>Open menu</span>
+              <MoreHorizontal className='h-4 w-4' />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            <DropdownMenuLabel></DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={
+                () => console.log(`Delete ${requirement.requirementId}`) // 함수 추가 후 콘솔 로그 삭제예정
+              }>
+              삭제하기
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={
+                () => console.log(`이전 버전확인 ${requirement.requirementId}`) // 함수 추가 후 콘솔 로그 삭제예정
+              }>
+              버전확인
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
     },
-    {
-      accessorKey: 'approvalStatus',
-      header: '승인여부',
-      cell: (info) => info.getValue(),
-    },
-    {
-      accessorKey: 'lockStatus',
-      header: '잠금여부',
-      cell: (info) => info.getValue(),
-    },
-    {
-      accessorKey: 'requirementId',
-      header: '항목ID',
-      cell: (info) => info.getValue(),
-    },
-    {
-      accessorKey: 'requirementName',
-      header: '요구사항이름',
-      cell: (info) => info.getValue(),
-    },
-    {
-      accessorKey: 'description',
-      header: '상세설명',
-      cell: (info) => info.getValue(),
-    },
-    {
-      accessorKey: 'assignee',
-      header: '담당자',
-      cell: (info) => info.getValue(),
-    },
-    {
-      accessorKey: 'author',
-      header: '작성자',
-      cell: (info) => info.getValue(),
-    },
-    {
-      accessorKey: 'currentVersion',
-      header: '현재버전',
-      cell: (info) => info.getValue(),
-    },
-    {
-      id: 'actions',
-      cell: ({ row }) => {
-        const requirement = row.original;
-        return (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant='ghost' className='h-8 w-8 p-0'>
-                <span className='sr-only'>Open menu</span>
-                <MoreHorizontal className='h-4 w-4' />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align='end'>
-              <DropdownMenuLabel></DropdownMenuLabel>
-              <DropdownMenuItem
-                onClick={() =>
-                  console.log(`Delete ${requirement.requirementId}`) // 함수 추가 후 콘솔 로그 삭제예정
-                }>
-                삭제하기
-              </DropdownMenuItem>
-              <DropdownMenuItem
-                onClick={() =>
-                  console.log(`이전 버전확인 ${requirement.requirementId}`) // 함수 추가 후 콘솔 로그 삭제예정
-                }>
-                버전확인
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        );
-      },
-    },
-  ]
+  },
+];
 
 // RequirementList: 특정 프로젝트의 요구사항을 개괄적으로 보는 컴포넌트
-const RequirementList = ({projectId, projectCode, projectName}: RequirementListProps) => {
+const RequirementList = ({
+  projectId,
+  projectCode,
+  projectName,
+}: RequirementListProps) => {
   // store에서 requirementId,requirementCode,requirementName를 업데이트 하는 함수를 가져옴
-  const { setRequirementId, setRequirementCode, setRequirementName } = usePageInfoStore()
+  // const { setRequirementId, setRequirementCode, setRequirementName } =
+  //   usePageInfoStore();
   // const { requirementId, requirementCode, requirementName } = usePageInfoStore.getState()
   // // 스토어에 잘 저장되었는지 체크
   // console.log('zustand 스토어에 잘 저장되었는지 체크')
@@ -203,39 +234,45 @@ const RequirementList = ({projectId, projectCode, projectName}: RequirementListP
               position: req.manager.position,
               role: req.manager.role,
               isApprovable: req.manager.isApprovable,
-              profileColor: req.manager.profileColor
+              profileColor: req.manager.profileColor,
             },
           }));
 
           // 첫번째 requirementId, requirementCode, requirementName를 가져와서 업데이트
-          setRequirementId(response.data[0].requirementId);
-          setRequirementCode(response.data[0].code);
-          setRequirementName(response.data[0].requirementName);
+          // setRequirementId(response.data[0].requirementId);
+          // setRequirementCode(response.data[0].code);
+          // setRequirementName(response.data[0].requirementName);
 
           setRequirements(requirementData);
         } else {
           setRequirements([]); // 데이터가 없는 경우 빈 배열 할당
         }
       } catch (error) {
-        console.error("Failed to fetch requirements:", error);
+        console.error('Failed to fetch requirements:', error);
         setRequirements([]); // 에러 처리
       }
     };
-  
+
     fetchRequirements();
-  }, [projectId, projectCode, setRequirementId, setRequirementCode, setRequirementName, setRequirements]);
+  }, [
+    projectId,
+    projectCode,
+    // setRequirementId,
+    // setRequirementCode,
+    // setRequirementName,
+    setRequirements,
+  ]);
 
-
-  const handleRowClick = (requirement:Requirement) => {
+  const handleRowClick = (requirement: Requirement) => {
     const { requirementId, code } = requirement;
     if (!requirementId) {
-      console.error('Invalid requirement data');  
+      console.error('Invalid requirement data');
       return; // 유효하지 않은 데이터에 대해 처리 중단
     }
     // console.log("requirementId : ", requirementId)
     // console.log("requirementCode : ", code)
     navigate(`/project/${projectCode}/requirement/${code}`, {
-      state: { requirementId }
+      state: { requirementId },
     });
   };
 
@@ -251,7 +288,9 @@ const RequirementList = ({projectId, projectCode, projectName}: RequirementListP
   return (
     <div className='p-8 text-center'>
       {/* 프로젝트 제목 및 기간 시작 */}
-      <p className='mb-4 text-2xl font-bold'>{ projectName  || '예시 프로젝트 제목' }</p>
+      <p className='mb-4 text-2xl font-bold'>
+        {projectName || '예시 프로젝트 제목'}
+      </p>
       <p className='mb-8 text-lg'>프로젝트 기간</p>
       {/* 프로젝트 제목 및 기간 끝 */}
       <br />
@@ -322,22 +361,18 @@ const RequirementList = ({projectId, projectCode, projectName}: RequirementListP
           <TableBody className='text-base'>
             {table.getRowModel().rows.length ? (
               table.getRowModel().rows.map((row) => (
-                
                 <TableRow
                   key={row.id}
                   role='button'
                   className='cursor-pointer hover:bg-gray-100'>
                   {row.getVisibleCells().map((cell) => (
-                    <TableCell 
+                    <TableCell
                       key={cell.id}
-                      onClick={()=> handleRowClick(row.original)}
-                      >
-                      
+                      onClick={() => handleRowClick(row.original)}>
                       {flexRender(
                         cell.column.columnDef.cell,
                         cell.getContext(),
                       )}
-                      
                     </TableCell>
                   ))}
                 </TableRow>
--- frontend/src/pages/main/MainPage.tsx
+++ frontend/src/pages/main/MainPage.tsx
@@ -1,38 +1,37 @@
 // 1. react 관련
-
+import { useEffect } from 'react';
 // 2. library
 // 3. api
 // 4. store
+import usePageInfoStore from '@/stores/pageInfoStore';
 // 5. components
-import 
-  ProjectList
-from "@/components/main/ProjectList";
-import 
-  Calendar
- from "@/components/main/Calendar";
+import ProjectList from '@/components/main/ProjectList';
+import Calendar from '@/components/main/Calendar';
 // 6. image 등 assets
 const MainDashboardPage = () => {
+  const { setPageType } = usePageInfoStore();
+
+  useEffect(() => {
+    setPageType('init');
+  }, []);
+
   return (
     <>
-    
-    <div className="flex justify-center">
-    <p className="text-5xl font-bold">프로젝트 대시보드 페이지</p>
-    </div>
-    {/* br보다 gap, margin, padding으로 마진 조정 */}
-    <div className="flex items-center justify-center ">
-      <div className="grid grid-cols-2 gap-10 max-w-[1500px] max-h-[900px] overflow-auto">
-
-        <div className="bg-gradient-to-r from-blue-500 to-blue-300 flex items-center justify-center text-black overflow-auto w-full h-full">
-          <Calendar />
-        </div>
-
-        <div className="flex items-center justify-center text-black overflow-auto w-full h-full">
-          <ProjectList />
-        </div>
-
+      <div className='flex justify-center'>
+        <p className='text-5xl font-bold'>프로젝트 대시보드 페이지</p>
       </div>
-    </div>
-    
+      {/* br보다 gap, margin, padding으로 마진 조정 */}
+      <div className='flex items-center justify-center '>
+        <div className='grid max-h-[900px] max-w-[1500px] grid-cols-2 gap-10 overflow-auto'>
+          <div className='flex h-full w-full items-center justify-center overflow-auto bg-gradient-to-r from-blue-500 to-blue-300 text-black'>
+            <Calendar />
+          </div>
+
+          <div className='flex h-full w-full items-center justify-center overflow-auto text-black'>
+            <ProjectList />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
