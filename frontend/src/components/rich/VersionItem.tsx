dc2413c24b9142d8668f8acc001b7a4c8a706249
김수빈 <ksb143@naver.com>
2024-05-16 15:23:17 +0900

    HOTFIX: 빌ë타입으로 인한 빌드 에러 수정 #9

--- frontend/src/components/rich/VersionItem.tsx
+++ frontend/src/components/rich/VersionItem.tsx
@@ -2,18 +2,20 @@ import { renderDate } from '@/utils/tiptap.ts';
 
 interface VersionItemProps {
   title?: string;
-  date: Date;
+  date: number;
   isActive: boolean;
   onClick: () => void;
-};
+}
 
 const VersionItem = ({ title, date, isActive, onClick }: VersionItemProps) => {
   return (
-    <button onClick={onClick} className={`appearance-none bg-transparent flex grow shrink text-white text-left hover:bg-pubble ${isActive && 'bg-pubble'}`}>
+    <button
+      onClick={onClick}
+      className={`flex shrink grow appearance-none bg-transparent text-left text-white hover:bg-pubble ${isActive && 'bg-pubble'}`}>
       <span>{title || renderDate(date)}</span>
       {title ? <span>{renderDate(date)}</span> : null}
     </button>
-  )
-}
+  );
+};
 
-export default VersionItem
+export default VersionItem;
