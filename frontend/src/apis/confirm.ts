997bff6ede7c91acd9c0687e7c77c6b9bd9b0deb
최지원 <jwchoi.v1@gmail.com>
2024-05-20 03:15:36 +0900

    Feat: 잠금 api 완료, 요구사항생성 api 완료, 승인 api 디버깅

--- frontend/src/apis/confirm.ts
+++ frontend/src/apis/confirm.ts
@@ -5,15 +5,23 @@ export const requestConfirm = async (
   reqId: number,
   requestBody: {
     projectId: number;
-    isLock: 'u' | 'l';
-    approval: 'u' | 'h' | 'a';
+    isLock: string;
+    approval: string;
     requirementName: string;
     approvalComment: string;
   },
 ) => {
+  // approvalComment를 '승인합니다'로 고정하여 요청 본문에 추가
+  const fullRequestBody = {
+    ...requestBody,
+    isLock: 'l',
+    approval: 'a',
+    approvalComment: '승인합니다',
+  };
+
   const response = await privateApi.post(
     `/requirements/confirm/${reqId}`,
-    requestBody,
+    fullRequestBody,
   );
   return response.data;
 };
