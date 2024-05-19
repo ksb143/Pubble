0fe315069184c6ad08f9850f9a662a5dcdd7fc4c
장준영 <wnsdud2889@naver.com>
2024-05-19 20:41:50 +0900

    Fix : StompEndpoints' setAllowedOriginPatterns

--- backend/src/main/java/com/ssafy/d109/pubble/config/WebSocketConfig.java
+++ backend/src/main/java/com/ssafy/d109/pubble/config/WebSocketConfig.java
@@ -12,14 +12,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/sub");  // 바로 일대다 브로드캐스팅
+        config.enableSimpleBroker("/sub");
         config.setApplicationDestinationPrefixes("/pub");  // 바로 브로커로 가지 않고 핸들러(Controller)로 라우팅
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws") // 클라이언트가 연결해야 할 엔드포인트
-                .setAllowedOrigins("*"); // cors 일단 다 허용
+                .setAllowedOriginPatterns("*"); // cors 일단 다 허용
 //                .withSockJS(); // 직접 지원 안하는 브라우저의 경우 sockjs를 들고 오면 됨
     }
 }
